7598f1b09d9a9ef12d6ffaf5e5d797a17818aff8
chunshin (cygwin) <l7263626@msn.com>
2013-12-26 05:23:21 +0000

    加入paymentType選項

--- conf/creditcard.php
+++ conf/creditcard.php
@@ -1,5 +1,6 @@
 <?php
 $cms_cfg['creditcard']['MerchantID'] = "2000132";//特店代碼
+$cms_cfg['creditcard']['PaymentType'] = "CREDIT";//信用卡固定CREDIT
 $cms_cfg['HashKey'] = "ejCk326UnaZWKisg";
 $cms_cfg['HashIV'] = "q9jcZX8Ib9LM8wYk";
 ?>

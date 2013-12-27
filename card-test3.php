bcce859a529066cfc3257ef208937def9f43c597
chunshin (cygwin) <l7263626@msn.com>
2013-12-27 06:25:23 +0000

    修改回傳結果頁

--- card-test3.php
+++ card-test3.php
@@ -9,14 +9,21 @@ include_once("libs/libs-mysql.php");
 $db = new DB($cms_cfg['db_host'],$cms_cfg['db_user'],$cms_cfg['db_password'],$cms_cfg['db_name'],$cms_cfg['tb_prefix']);
 $tpl = new TemplatePower("test3.html");
 $tpl->prepare();
-$card = new Model_Order_Payment_Esun($cms_cfg['creditcard'], $cms_cfg['esunkey']);
-$sql = $card->update_order($db,$_GET);
+/*初始化payment物件*/
+$card = new Model_Order_Payment_Allpay($cms_cfg['creditcard'], $cms_cfg['Hash']);
+/*解析回傳結果*/
+$xmldata = mcrypt_aes::aes128cbcDecrypt($_POST['XMLData'], $cms_cfg['Hash']['IV'], $cms_cfg['Hash']['Key']);
+$returnXML = new SimpleXMLElement($xmldata);
+/*更新訂單*/
+$sql = $card->update_order($db,$returnXML);
+/*輸出回傳結果*/
 $tpl->gotoBlock("_ROOT");
 $tpl->assign("UPDATE_ORDER_SQL",$sql);
-foreach($_GET as $k=>$v){
-    $tpl->assign("MSG_".strtoupper($k),$v);
-    if($k=="RC"){
-        $tpl->assign("MSG_".strtoupper($k)."_STR",  Model_Order_Payment_Returncode_Esun::$code[$_GET[$k]]);
+foreach($returnXML->Data->children() as $elm){
+    $tpl->assign("MSG_".strtoupper($elm->getName()),$elm);
+    if($elm->getName()=="RtnCode"){
+        $idx = sprintf("%s",$elm);
+        $tpl->assign("MSG_".strtoupper($elm->getName())."_STR",  Model_Order_Payment_Returncode_Allpay::$code[$idx]);
     }
 }
 $tpl->printToScreen();

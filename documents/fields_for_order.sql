1eaa9807f5f000f09c1262ecd818746c6142e46a
chunhsin,lin <l7263626@msn.com>
2013-12-31 15:31:34 +0800

    new documents/fields_for_order.sql

---
+++ documents/fields_for_order.sql
@@ -0,0 +1,5 @@
+ALTER TABLE  `cht_order` ADD  `TradeNo` VARCHAR( 20 ) NOT NULL ,
+ADD  `RtnCode` INT NOT NULL ,
+ADD  `gwsr` INT NOT NULL ,
+ADD  `process_date` DATETIME NOT NULL ,
+ADD  `auth_code` VARCHAR( 6 ) NOT NULL ;
